57a899d1d87926e1e39577cdb575740d3017f90f
figsoda <figsoda@pm.me>
2026-02-04 21:53:27 -0500

    store: fix race conditions with multiple instances of unnix

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,7 +1,7 @@
 pub mod nar;
 pub mod path;
 
-use std::{fs::create_dir_all, io::Cursor, num::NonZero, rc::Rc};
+use std::{fs::create_dir_all, io::Cursor, num::NonZero, rc::Rc, time::Duration};
 
 use async_compression::tokio::bufread::{
     BrotliDecoder, BzDecoder, GzipDecoder, Lz4Decoder, LzmaDecoder, XzDecoder, ZstdDecoder,
@@ -9,30 +9,41 @@ use async_compression::tokio::bufread::{
 use camino::Utf8PathBuf;
 use derive_more::AsRef;
 use dirs::cache_dir;
+use fs4::tokio::AsyncFileExt;
 use miette::{IntoDiagnostic, Result, miette};
 use nix_nar::Decoder;
 use tokio::{
+    fs::File,
     io::{AsyncBufRead, AsyncReadExt},
     task::spawn_blocking,
+    time::sleep,
 };
 
 use crate::store::{nar::Compression, path::StorePath};
 
 #[derive(AsRef, Clone)]
-#[as_ref(forward)]
 pub struct Store {
+    #[as_ref(forward)]
     path: Rc<Utf8PathBuf>,
+    lock: Utf8PathBuf,
 }
 
 impl Store {
     pub fn new() -> Result<Self> {
         let cache = cache_dir().ok_or_else(|| miette!("no cache directory found"))?;
-        let path = Utf8PathBuf::try_from(cache)
+        let cache = Utf8PathBuf::try_from(cache)
             .into_diagnostic()?
-            .join("unnix/store");
+            .join("unnix");
+
+        let path = cache.join("store");
+        let lock = cache.join("lock");
+
         create_dir_all(&path).into_diagnostic()?;
+        create_dir_all(&lock).into_diagnostic()?;
+
         Ok(Store {
             path: Rc::new(path),
+            lock,
         })
     }
 
@@ -95,7 +106,16 @@ impl Store {
             }
         }
 
+        let lock = File::create(self.lock.join(path)).await.into_diagnostic()?;
+        while !lock.try_lock_exclusive().into_diagnostic()? {
+            sleep(Duration::from_millis(250)).await;
+        }
+
         let path = self.path.join(path);
+        if path.exists() {
+            return Ok(());
+        }
+
         spawn_blocking(|| {
             Decoder::new(Cursor::new(buf))
                 .into_diagnostic()?
