adf46c06073c8794d7cdbca2abc3b607f4ab6770
figsoda <figsoda@pm.me>
2026-03-02 23:29:51 -0500

    don't try to use bubblewrap on darwin

--- src/command/env.rs
+++ src/command/env.rs
@@ -1,6 +1,7 @@
-use std::{env::var_os, os::unix::process::CommandExt};
+use std::{env::var_os, os::unix::process::CommandExt, process::Command};
 
-use miette::{Report, Result};
+use miette::{Report, Result, WrapErr};
+use tracing::warn;
 
 use crate::{
     cli::{EnvArgs, GlobalArgs},
@@ -11,16 +12,35 @@ pub async fn env(global: GlobalArgs, args: EnvArgs) -> Result<()> {
     let mut state = State::new(global)?;
     state.lock().await?;
 
-    let mut cmd = state.bwrap();
-    cmd.envs(state.env().await?);
-
-    if let Some(args) = args.command {
-        cmd.args(args);
-    } else if let Some(shell) = var_os("SHELL") {
-        cmd.arg(shell);
+    let mut cmd = if cfg!(target_os = "linux") {
+        let mut cmd = state.bwrap();
+        if let Some(args) = args.command {
+            cmd.args(args);
+        } else if let Some(shell) = var_os("SHELL") {
+            cmd.arg(shell);
+        } else {
+            cmd.arg("sh");
+        }
+        cmd
     } else {
-        cmd.arg("sh");
-    }
+        // TODO: hook into libSystem to redirect paths
+        warn!(
+            "`unnix env` is only properly supported on linux, please mount or symlink {} it to /nix/store manually",
+            state.store.path,
+        );
 
+        if let Some(args) = args.command {
+            let mut args = args.into_iter();
+            let mut cmd = Command::new(args.next().wrap_err("expected at least one argument")?);
+            cmd.args(args);
+            cmd
+        } else if let Some(shell) = var_os("SHELL") {
+            Command::new(shell)
+        } else {
+            Command::new("sh")
+        }
+    };
+
+    cmd.envs(state.env().await?);
     Err(Report::from_err(cmd.exec()))
 }
