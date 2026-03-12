c6ff59e78ded2c1c94b16c92dd8c8f1c6f63274d
figsoda <figsoda@pm.me>
2026-03-12 15:07:47 -0400

    update: progress bar

--- src/command/update.rs
+++ src/command/update.rs
@@ -2,11 +2,18 @@ use std::rc::Rc;
 
 use miette::{IntoDiagnostic, Result};
 use tokio::task::{JoinSet, LocalSet};
+use tracing::{field::Empty, info_span};
+use tracing_indicatif::span_ext::IndicatifSpanExt;
 
 use crate::{cli::GlobalArgs, lockfile::SystemLockfile, state::State};
 
 pub async fn update(global: GlobalArgs) -> Result<()> {
     let mut state = State::new(global)?;
+    let span = info_span!("update", indicatif.pb_show = Empty);
+    span.pb_set_message("updating lockfile");
+    span.pb_set_length(0);
+    span.pb_start();
+
     let local = LocalSet::new();
     let mut tasks = JoinSet::new();
 
@@ -14,8 +21,14 @@ pub async fn update(global: GlobalArgs) -> Result<()> {
         let lockfile = Rc::new(SystemLockfile::default());
         for (name, pkg) in manifest.packages {
             let lockfile = lockfile.clone();
+            let span = span.clone();
             tasks.spawn_local_on(
-                async move { lockfile.fetch(system, name, &pkg).await },
+                async move {
+                    span.pb_inc_length(1);
+                    lockfile.fetch(system, name, &pkg).await?;
+                    span.pb_inc(1);
+                    Result::<_>::Ok(())
+                },
                 &local,
             );
         }
